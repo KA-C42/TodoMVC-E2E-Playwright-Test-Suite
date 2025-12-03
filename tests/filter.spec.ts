cfc0f423684d375db48ef59c58cdd8f83a5f069d
emSoTired <kacyCote42@gmail.com>
2025-12-02 18:19:25 -0800

    test: F1-F3, add filter buttons to POM

--- tests/filter.spec.ts
+++ tests/filter.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test'
 import { TodoMvcPage } from './pages/todomvc-page'
+import { populateVaried } from './utils/seederHelpers'
 
 test.describe('TodoMVC - Filtering', () => {
   let todoPage: TodoMvcPage
@@ -9,5 +10,56 @@ test.describe('TodoMVC - Filtering', () => {
 
     // go to starting URL before each test
     await todoPage.goto()
+
+    await populateVaried(todoPage)
+  })
+
+  // F1
+  test('filter to show active items only', async () => {
+    // SETUP
+    const totalTodos = await todoPage.todoItems.count()
+    const activeTodos = await todoPage.getActiveCount()
+    const completedTodos = totalTodos - activeTodos
+    expect(completedTodos).toBeGreaterThan(0) // confirm todos should be filtered out
+
+    await todoPage.activeFilter.click()
+
+    // ASSERTIONS
+    await expect(todoPage.page).toHaveURL(/\/todomvc\/#\/active/)
+    await expect(todoPage.todoItems).toHaveCount(activeTodos)
+    for (let i = 0; i < activeTodos; i++)
+      await expect(todoPage.todoItems.nth(i)).not.toHaveClass('completed')
+  })
+
+  // F2
+  test('filter to show completed items only', async () => {
+    // SETUP
+    const totalTodos = await todoPage.todoItems.count()
+    const activeTodos = await todoPage.getActiveCount()
+    const completedTodos = totalTodos - activeTodos
+    expect(completedTodos).toBeGreaterThan(0) // confirm todos should be filtered out
+
+    await todoPage.completedFilter.click()
+
+    // ASSERTIONS
+    await expect(todoPage.page).toHaveURL(/\/todomvc\/#\/completed/)
+    await expect(todoPage.todoItems).toHaveCount(completedTodos)
+    for (let i = 0; i < completedTodos; i++)
+      await expect(todoPage.todoItems.nth(i)).toHaveClass('completed')
+  })
+
+  // F3
+  test('navigate to full list view from filtered view', async () => {
+    // SETUP
+    const totalTodos = await todoPage.todoItems.count()
+
+    await todoPage.activeFilter.click()
+    await expect(todoPage.page).toHaveURL(/\/todomvc\/#\/active/)
+
+    await todoPage.allFilter.click()
+
+    // ASSERTIONS
+    await expect(todoPage.page).toHaveURL(/\/todomvc\/#\//)
+    await expect(todoPage.todoItems).toHaveCount(totalTodos)
   })
 })

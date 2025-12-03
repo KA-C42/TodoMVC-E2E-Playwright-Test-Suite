b58989f3468b5564f5d72f2772f0b9d11b1638ff
emSoTired <kacyCote42@gmail.com>
2025-12-03 15:54:53 -0800

    test: S2, data persists on refresh

--- tests/sanity.spec.ts
+++ tests/sanity.spec.ts
@@ -23,4 +23,48 @@ test.describe('TodoMVC - Sanity checks', () => {
     // empty state: no todos, no toggle-all checkbox, no footer
     await expectEmptyState(todoPage)
   })
+
+  // S2
+  test('state and data persist on refresh', async () => {
+    const toSaves = ['tod0', 'tod1', 'tod2']
+
+    // mix the order of active/complete todos
+    for (const [index, todo] of toSaves.entries()) {
+      if (index % 2 === 0) {
+        await todoPage.addTodo(todo)
+      } else {
+        await todoPage.addAndCompleteTodo(todo)
+      }
+    }
+    const startingTotal = await todoPage.todoItems.count()
+    expect(startingTotal).toBe(toSaves.length)
+
+    await todoPage.activeFilter.click()
+    await expect(todoPage.page).toHaveURL(/\/todomvc\/#\/active/)
+
+    await todoPage.page.reload()
+
+    // ASSERTIONS
+    await expect(todoPage.page).toHaveURL(/\/todomvc\/#\/active/)
+    await todoPage.allFilter.click()
+    await expect(todoPage.page).toHaveURL(/\/todomvc\/#/)
+
+    // check the order of active/complete persisted
+    for (const [index, todo] of toSaves.entries()) {
+      // ensure index has correct name
+      await expect(todoPage.todoItems.nth(index)).toHaveText(todo)
+
+      // ensure name has correct completion
+      if (index % 2 === 0) {
+        await expect(todoPage.getTodoItem(todo).root).not.toHaveClass(
+          'completed',
+        )
+      } else {
+        await expect(todoPage.getTodoItem(todo).root).toHaveClass('completed')
+      }
+    }
+
+    const endingTotal = await todoPage.todoItems.count()
+    expect(endingTotal).toBe(startingTotal)
+  })
 })
